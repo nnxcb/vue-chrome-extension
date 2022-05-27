9b5d5430680af67205620d60664002edbc0e095b
nnxcb <45084717+nnxcb@users.noreply.github.com>
2022-05-27 10:37:50 +0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -3,10 +3,26 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
+let minute = 30;
 export default new Vuex.Store({
   state: {
+    drinkInfo: {
+      open: true,
+      interval: minute,
+    },
+    countDown: minute * 60,
   },
   mutations: {
+    changeDinkInfo(state, info) {
+      state.drinkInfo = Object.assign({}, state.drinkInfo, info);
+      setTimeout(() => {
+        browser.runtime.sendMessage({type: 'resetDrink', info: state.drinkInfo});
+      }, 1000);
+    },
+    changeCountDown(state, num) {
+      state.countDown = num;
+      console.log('changeCountDown', state)
+    }
   },
   actions: {
   },
