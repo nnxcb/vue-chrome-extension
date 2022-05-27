343a59713e85486498cbd5f37850755792f96f32
nnxcb <45084717+nnxcb@users.noreply.github.com>
2022-05-27 16:20:57 +0800

    Update tool.js

--- utils/tool.js
+++ utils/tool.js
@@ -1,6 +1,6 @@
 import dayjs from "dayjs";
-import store from '../src/store';
-import logo from '../src/assets/logo.png';
+// import logo from '../src/assets/logo.png';
+import { minute } from './const';
 
 const WEEKS = {
   1: '星期一',
@@ -17,31 +17,40 @@ export const weekToday = () => {
   return WEEKS[week]
 }
 
+// 初始化喝水参数
+export function initDrinkState() {
+  const drinkState = {
+    open: true,
+    interval: minute,
+  }
+  chrome.storage.local.set({drinkState},function(){
+    console.log('初始化喝水参数');
+  });
+}
+
 // 开启定时任务提醒
 // eslint-disable-next-line no-unused-vars
 let drinkInterval;
-export function resetDrink(info = store.state.drinkInfo) {
-  const { interval, open } = info;
-  if (!open && drinkInterval) {
-    clearInterval(drinkInterval);
-    return;
-  }
-  if (drinkInterval) {
-    clearInterval(drinkInterval);
-  }
-  let time = interval * 60 * 1000;
-  chrome.storage.local.set({noticeTime: new Date().valueOf() + time},function(){
-    console.log('set成功', time)
-  });
-  drinkInterval = setInterval(() => {
-    console.log('触发提示');
-    chrome.storage.local.set({noticeTime: new Date().valueOf() + time},function(){});
-    chrome.notifications.create(null, {
-      type: "image",
-      iconUrl: logo,
-      imageUrl: logo,
-      title: "提醒喝水小助手",
-      message: `看到此消息的人可以和我一起来喝一杯水。及时排便洗手，记得关门。后的我继续提醒大家喝水。和我一起成为一天八杯水的人吧！`,
+export function resetDrink() {
+  chrome.storage.local.get(['drinkState'], function(result) {
+    const info = result.drinkState;
+    console.log('触发重置', info)
+    const { interval, open } = info;
+    if (!open && drinkInterval) {
+      clearInterval(drinkInterval);
+      return;
+    }
+    if (drinkInterval) {
+      clearInterval(drinkInterval);
+    }
+    let time = interval * 60 * 1000;
+    chrome.storage.local.set({noticeTime: new Date().valueOf() + time},function(){
+      console.log('set成功', time)
     });
-  }, time);
+    // 创建喝水定时任务
+    chrome.alarms.create('test', {
+      // 间隔时间，单位为分钟
+      periodInMinutes: interval
+    })
+  });
 }
